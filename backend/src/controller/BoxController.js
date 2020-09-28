c5ee7182a05d58e4e2266b438f0443ce302f55f7
Luciano WRibeiro <lucianowribeiro@gmail.com>
2020-09-28 12:20:00 -0300

    🎨 Remove useless files

--- backend/src/controller/BoxController.js
+++
@@ -1,9 +0,0 @@
-const Box = require('../models/Box')
-class BoxController {
-    async store(req,res){
-        const box = await Box.create({title: req.body.title})
-        return res.json(box)
-    }
-}
-const box = new BoxController()
-module.exports = box 
